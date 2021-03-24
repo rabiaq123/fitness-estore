ad4b527280923e860f2c3e624e32327be8bb678b
mbasak99 <monarkb01@gmail.com>
2021-03-24 18:56:44 -0400

    Made progress on the products page, just need to fix the grid sizes and stuff. Still need to work on the navbar, it's getting there.

--- client/src/components/screens/Products/Products.js
+++ client/src/components/screens/Products/Products.js
@@ -1,9 +1,59 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { Paper } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { Grid, Box } from "grommet";
 
-export default function Products() {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+    "& > *": {
+      margin: theme.spacing(1),
+      width: theme.spacing(16),
+      height: theme.spacing(16),
+    },
+  },
+}));
+
+export default function Products({ category, search }) {
+  const classes = useStyles();
+
+  useEffect(
+    () => {
+      // Call DB here for products
+      // Filter by
+      // return () => {};
+    },
+    [
+      /* Track category, filter, and sort by values (left accordion)*/
+    ]
+  );
+
   return (
-    <div>
-      Products
-    </div>
-  )
+    <Grid
+      justify="stretch"
+      fill="horizontal"
+      rows={["medium", "medium"]}
+      columns={["medium", "large"]}
+      gap="large"
+      areas={[
+        { name: "left-options", start: [0, 0], end: [0, 1] },
+        { name: "products-container", start: [1, 0], end: [1, 1] },
+      ]}
+      // style={{ margin: "auto" }}
+    >
+      <Box gridArea="left-options">
+        <div style={{ backgroundColor: "pink" }}></div>
+      </Box>
+      <Box gridArea="products-container">
+        <Paper
+          style={{ backgroundColor: "grey", height: "100%", width: "100%" }}
+          className={classes.root}
+          elevation={0}
+        >
+          Test
+        </Paper>
+      </Box>
+    </Grid>
+  );
 }
--- client/src/components/utils/Navbar/Navbar.js
+++ client/src/components/utils/Navbar/Navbar.js
@@ -1,13 +1,63 @@
 import React from "react";
-import { AppBar, InputBase, Toolbar } from "@material-ui/core";
+import fitnova from "../../../assets/fitnova_logo.png";
+import { AppBar, InputBase, Toolbar, Typography } from "@material-ui/core";
+import { fade, makeStyles } from "@material-ui/core/styles";
+import { Link } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  grow: {
+    flexGrow: 1,
+  },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(3),
+      width: "auto",
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputRoot: {
+    color: "inherit",
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
+    },
+  },
+}));
 
 export default function Navbar() {
+  const classes = useStyles();
+
   return (
-    <AppBar position="static">
-      <Toolbar style={{ backgroundColor: "#FE646F" }}>
-        {/* <img src="" /> */}
-        <InputBase placeholder="Search..." />
-      </Toolbar>
-    </AppBar>
+    <div className={classes.grow}>
+      <AppBar position="static">
+        <Toolbar style={{ backgroundColor: "#FE646F", padding: 20 }}>
+          <img height="50px" width="auto" src={fitnova} alt="Fitnova logo" />
+          <Typography>Category</Typography>
+          <InputBase placeholder="Search..." />
+        </Toolbar>
+      </AppBar>
+    </div>
   );
 }

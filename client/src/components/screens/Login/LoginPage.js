c04f20fe23db8fdb43c3088beef1f02fdcaa2f78
SachJay <jayaweerasachin@gmail.com>
2021-03-28 12:19:54 -0400

    made structure for Ui of login page

--- client/src/components/screens/Login/LoginPage.js
+++ client/src/components/screens/Login/LoginPage.js
@@ -1,9 +1,56 @@
 import React from "react";
+import { Box, Button, TextInput } from "grommet";
+import { Link } from "@material-ui/core"
 
 function LoginPage() {
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
   return (
     <div>
-        <p>Replace this with code</p>
+        <h1>Welcome</h1>
+        <img src="fitnova_icon.png"></img><br/>
+
+        <Box align="center" background="white">
+        <TextInput
+          placeholder="Email"
+          value={email}
+          onChange={event => setEmail(event.target.value)}
+        />
+
+        <TextInput
+          placeholder="Password"
+          value={password}
+          onChange={event => setPassword(event.target.value)}
+        />
+
+      
+        <Button
+            label="Login"
+            primary 
+            onClick={() => alert('Login')}
+        />
+        </Box>
+
+        <Link
+          component="button"
+          variant="body2"
+          onClick={() => {
+            alert("Take me to reset password"); //TODO
+          }}
+        >
+          Forgot your password?
+        </Link><br/>
+
+        <Link
+          component="button"
+          variant="body2"
+          onClick={() => {
+            alert("Take me to register"); //TODO
+          }}
+        >
+          Don’t have an account? Sign up!
+        </Link><br/>
     </div>
   );
 }

1e541d0143fdc59b28602d37bda3bdbaa92bddbd
Monark Basak <monarkb01@gmail.com>
2021-03-27 20:34:01 -0400

    Updated my progress on products page.

--- client/src/components/screens/Products/Products.js
+++ client/src/components/screens/Products/Products.js
@@ -9,6 +9,8 @@ import {
 import { makeStyles } from "@material-ui/core/styles";
 import { Grid, Box } from "grommet";
 import ProductDetails from "../ProductDetails/ProductDetails";
+import axios from "axios";
+import { useLocation } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   product_container: {
@@ -19,7 +21,7 @@ const useStyles = makeStyles((theme) => ({
     //   width: theme.spacing(16),
     //   height: theme.spacing(16),
     // },
-    backgroundColor: "grey",
+    backgroundColor: "#EFF0F6",
     height: "100%",
     maxWidth: "1200px",
     borderRadius: 50,
@@ -35,33 +37,55 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const ProductCard = (productItem) => {
+  // TODO
   return <div></div>;
 };
 
-export default function Products({ category, search }) {
+export default function Products() {
   const classes = useStyles();
-
+  const location = useLocation();
+  const payload = {
+    code: 200,
+    token: "6C3ccXkMYv4Vok_Huxptwg",
+    data: {
+      price: "nameFirst",
+      email: "internetEmail",
+      phone: "phoneHome",
+      // _repeat: 1,
+    },
+  };
   const [products, setProducts] = useState([]);
 
   useEffect(
     () => {
       // For testing purposes
-      setProducts([
-        {
-          id: 1,
-          price: 199.99,
-          name: "Testing Dumbbell",
-          image_url: "bowflex-selecttech-552-dumbbell-set.png",
-        },
-      ]);
+      // (async function getProducts() {
+      //   axios
+      //     .post("https://app.fakejson.com/q", payload)
+      //     .then((resp) => {
+      //       setProducts(resp.data);
+      //     })
+      //     .catch((err) => {
+      //       console.log(err);
+      //     });
+      // })();
+      // setProducts([
+      //   {
+      //     id: 1,
+      //     price: 199.99,
+      //     name: "Testing Dumbbell",
+      //     image_url: "bowflex-selecttech-552-dumbbell-set.png",
+      //   },
+      // ]);
       // Call DB here for products
       // Filter by
       // return () => {};
-    },
-    [
-      /* Track category, filter, and sort by values (left accordion) */
-    ]
+    }
+    // [
+    //   /* Track category, filter, and sort by values (left accordion) */
+    // ]
   );
+  console.log(location);
 
   return (
     <Grid
@@ -77,12 +101,12 @@ export default function Products({ category, search }) {
       style={{ margin: 50, marginLeft: 50, marginRight: 100 }}
     >
       <Box gridArea="left-options">
-        {/* <div style={{ backgroundColor: "pink" }}></div> */}
         <Paper
           style={{
-            backgroundColor: "grey",
+            backgroundColor: "#EFF0F6",
             height: "100%",
             width: "100%",
+            borderRadius: 50,
           }}
           elevation={0}
         ></Paper>
@@ -91,7 +115,7 @@ export default function Products({ category, search }) {
         <Paper className={classes.product_container} elevation={0}>
           {products.length ? (
             products.map((item, index) => {
-              return <ProductCard id={index} {...item} />;
+              return <ProductCard key={index} {...item} />;
             })
           ) : (
             <></>

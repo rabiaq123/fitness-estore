c4d3f172d6d1e5e6ae084516928852211fc82eec
mbasak99 <monarkb01@gmail.com>
2021-03-29 23:36:08 -0400

    Moved Navbar into Router file in order to be able to use Link component properly.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import "./App.css";
 import Footer from "./components/utils/Footer/Footer";
-import Navbar from "./components/utils/Navbar/Navbar";
 import Router from "./Router";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
       <Router />
       <Footer />
     </div>
--- client/src/Router.js
+++ client/src/Router.js
@@ -4,11 +4,13 @@ import LoginPage from "./components/screens/Login/LoginPage";
 import LandingPage from "./components/screens/LandingPage/LandingPage";
 import Products from "./components/screens/Products/Products";
 import ProductDetails from "./components/screens/ProductDetails/ProductDetails";
+import Navbar from "./components/utils/Navbar/Navbar";
 
 export default function Router() {
   return (
     <div>
       <BrowserRouter>
+        <Navbar />
         <Switch>
           <Route path="/" exact component={LandingPage} />
           <Route path="/products" exact component={Products} />

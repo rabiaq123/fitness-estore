ef0df3a00d32da27f70d47eda9bb10a93768b679
darrenchay <darrenchay@gmail.com>
2021-03-30 15:43:15 -0400

    changed server local port

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log(`app is listening on port: ${port}`);
 });

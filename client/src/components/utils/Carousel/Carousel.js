ddeb653f0b3eb75ff562030cdbcf6201d6160d5a
mbasak99 <monarkb01@gmail.com>
2021-04-09 15:50:28 -0400

    Added new guy for carousel.

--- client/src/components/utils/Carousel/Carousel.js
+++ client/src/components/utils/Carousel/Carousel.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Carousel as LandingCarousel, Image } from "grommet";
 // @ts-ignore
 import guy from "../../../assets/landing_imgs/black_guy_lifting.png";
+import white_guy from "../../../assets/landing_imgs/man-lifting-weight.jpg";
 
 export default function Carousel() {
   document.title = "Home";
@@ -9,7 +10,7 @@ export default function Carousel() {
     // Remember to change the carousel minus colour at the bottom to theme colour
     <LandingCarousel play={15000} fill alignSelf="center">
       <Image fit="contain" src={guy} />
-      <Image fit="contain" src={guy} />
+      <Image fit="contain" src={white_guy} />
     </LandingCarousel>
   );
 }
